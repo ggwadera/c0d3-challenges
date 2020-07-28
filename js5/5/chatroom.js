82c1c798d67c93062ba55743a06692098d355c7c
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-07-28 11:56:39 -0300

    improve documentation

--- js5/5/chatroom.js
+++ js5/5/chatroom.js
@@ -2,11 +2,12 @@ const fetch = require("node-fetch")
 
 const rooms = {}
 
-const Message = function (name, message) {
-  this.name = name
-  this.message = message
-}
-
+/**
+ * Authenticates the user session from the JWT using a third-party server.
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @param {Function} next - Next route in the path
+ */
 const getUserMiddleware = (req, res, next) => {
   const jwt = req.headers.authorization
   fetch("https://js5.c0d3.com/auth/api/session", {
@@ -24,10 +25,15 @@ const getUserMiddleware = (req, res, next) => {
           password: body.password,
         }
       }
-      next()
+      next() // calls the next route
     })
 }
 
+/**
+ * Validates the user session
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const getSession = (req, res) => {
   if (!req.user) {
     return res.status(403).json({error: "Invalid session"})
@@ -35,6 +41,11 @@ const getSession = (req, res) => {
   res.json(req.body)
 }
 
+/**
+ * Sends a JSON with all the messages of the queried room
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const getMessages = (req, res) => {
   const room = req.params.room
   if (!rooms[room]) {
@@ -44,12 +55,17 @@ const getMessages = (req, res) => {
   return res.json(rooms[room])
 }
 
+/**
+ * Posts a new message in the specified room
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const postMessage = (req, res) => {
   const room = req.params.room
   if (!rooms[room]) {
     rooms[room] = []
   }
-  rooms[room].push(new Message(req.user.name, req.body.message))
+  rooms[room].push({name: req.user.name, message: req.body.message})
   console.log(`${new Date().toString()} chatroom: new message in room ${room} from ${req.user.name}`)
   res.sendStatus(201)
 }
