e9ba597f5346c281a0bbc4bbf963b815423fdec7
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-07-28 11:49:12 -0300

    update documentation

--- js5/4/assets.js
+++ js5/4/assets.js
@@ -5,10 +5,20 @@ const FILE_LIFETIME = 1000 * 60 * 5 // 5 minutes
 const path = __dirname + "/files/"
 const files = {} // store files and last modified time
 
+/**
+ * Sends a JSON with all the files info
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const findAll = (req, res) => {
   res.send(JSON.stringify(Object.keys(files)))
 }
 
+/**
+ * Sends back the contents of the file in the request parameter
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const findFile = (req, res) => {
   const filename = req.params.filename
   console.log(`${new Date().toString()} assets: fetching file ${filename}`)
@@ -21,6 +31,11 @@ const findFile = (req, res) => {
   })
 }
 
+/**
+ * Writes a new file on the server with the name and contents in the request body
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const createFile = (req, res) => {
   const filename = req.body.filename
   const content = req.body.content
@@ -31,6 +46,9 @@ const createFile = (req, res) => {
   })
 }
 
+/**
+ * Deletes files older than FILE_LIFETIME
+ */
 const deleteOldFiles = () => {
   if (Object.keys(files).length > 0) {
     const maxTime = Date.now() - FILE_LIFETIME
@@ -46,6 +64,9 @@ const deleteOldFiles = () => {
   }
 }
 
+/**
+ * Initializes the files object with the files that already exists
+ */
 const initialize = () => {
   fs.readdir(path, (err, data) => {
     if (!data) return
