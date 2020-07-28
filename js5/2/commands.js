17f39a0d645e8c9f3b75742dec4e17175fcf2df2
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-07-28 11:37:43 -0300

    improve documentation

--- js5/2/commands.js
+++ js5/2/commands.js
@@ -1,11 +1,22 @@
 const { execFile } = require("child_process")
 
+/**
+ * List of allowed commands to run on the server
+ */
 const allowedCommands = {
   ls: true,
   pwd: true,
   cat: true,
 }
 
+/**
+ * Runs the command on the server with Node.js's child_process.execFile,
+ * this way, there's less risk of command injection with "&&" or similar,
+ * because execFile takes the command as the first parameter and inteprets
+ * the remaining commands as arguments to the command.
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ */
 const runCommand = (req, res) => {
   const input = req.body.command.split(" ")
   const command = input.shift()
